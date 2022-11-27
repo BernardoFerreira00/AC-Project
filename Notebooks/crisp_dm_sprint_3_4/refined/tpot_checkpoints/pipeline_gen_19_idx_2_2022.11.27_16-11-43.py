c7c729898e1d3cd1cb1192e7d3e305f5651c3b3c
wolfCuanhamaRWS <up201509918@fc.up.pt>
2022-11-27 17:31:20 +0000

    A model with AUC = 0.8222

---
+++ Notebooks/crisp_dm_sprint_3_4/refined/tpot_checkpoints/pipeline_gen_19_idx_2_2022.11.27_16-11-43.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from tpot.export_utils import set_param_recursive
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=42)
+
+# Average CV score on the training set was: 0.7831501831501833
+exported_pipeline = make_pipeline(
+    RFE(estimator=ExtraTreesClassifier(criterion="entropy", max_features=0.35000000000000003, n_estimators=100), step=0.7000000000000001),
+    PCA(iterated_power=1, svd_solver="randomized"),
+    SGDClassifier(alpha=0.0, eta0=0.1, fit_intercept=False, l1_ratio=0.75, learning_rate="constant", loss="log", penalty="elasticnet", power_t=0.0)
+)
+# Fix random state for all the steps in exported pipeline
+set_param_recursive(exported_pipeline.steps, 'random_state', 42)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
